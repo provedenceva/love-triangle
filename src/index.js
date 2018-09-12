94588b51b29bf95adc03c080569202497155f170
provedenceva <anyuta-provedenc@mail.ru>
2018-09-13 00:14:17 +0300

    task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  preferences.unshift(0);
+  var triangles = 0;
+  var a;
+  var b;
+  var c;
+  for(let i = 1; i < preferences.length; i++){
+    if (preferences[i] == 0) continue;
+    a = preferences[i];
+    b = preferences[a];
+    c = preferences[b];
+    if(a == b || b == c ) continue;
+    if (c == i) {
+      triangles++;
+    
+    }
+    }
+ return (triangles/3); // your implementation
+}
